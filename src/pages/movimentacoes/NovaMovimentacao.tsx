db2b244ffce822a99ae5cfa601e00ffdc0bdc207
sampaiolucasdev <lsampaio32@gmail.com>
2022-08-18 16:57:14 -0300

    Feat: tabela de movimentação criada

--- src/pages/movimentacoes/NovaMovimentacao.tsx
+++ src/pages/movimentacoes/NovaMovimentacao.tsx
@@ -30,7 +30,6 @@ import {
   SetorService,
 } from "../../shared/services/api/SetorService";
 import { useDebounce } from "../../shared/hooks";
-import { width } from "@mui/system";
 
 interface IFormData {
   id?: number;
@@ -93,7 +92,49 @@ export const NovaMovimentacao: React.FC = () => {
     value: row.id,
     label: row.nome,
   }));
-  const selectStyles = { width: `${8 * mapSetor.length + 100}px` };
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "firstName",
+      headerName: "First name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "lastName",
+      headerName: "Last name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 110,
+      editable: true,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (params: GridValueGetterParams) =>
+        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    },
+  ];
+  // const selectStyles = { width: `${8 * mapSetor.length + 100}px` };
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
 
   const handleSave = (dados: IFormData) => {
     formValidationSchema
@@ -266,8 +307,17 @@ export const NovaMovimentacao: React.FC = () => {
           flexDirection="column"
           component={Paper}
           variant="outlined"
+          sx={{ height: 400, width: "100%" }}
         >
-          <DataGrid />
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
+            disableSelectionOnClick
+            experimentalFeatures={{ newEditingApi: true }}
+          />
         </Box>
         {/* {[1, 2, 3, 4].map((_, index) => (
           <Scope key="" path={`endereco[${index}]`}>
